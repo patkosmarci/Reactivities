c8e439efea1c8932cc53692c5503377177980a2c
patkosmarci <patkosmarci@gmail.com>
2021-03-02 12:43:17 +0100

    End of Section 9

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -8,13 +8,13 @@ export default function NavBar(){
     return(
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item as={NavLink} to="/" exact header>
+                <Menu.Item as={NavLink} to="/" exact header >
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
                     Reactivities
+                </Menu.Item>
                 <Menu.Item as={NavLink} to="/activities" name="Activities"/>
                 <Menu.Item>
                     <Button as={NavLink} to="/createActivity" positive content='Create Activity'/>
-                </Menu.Item>
                 </Menu.Item>
             </Container>
         </Menu>
